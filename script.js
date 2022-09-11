e95f4a5abfb2dd35d704dcb880c83fe2c234f230
Tushar <singhtushar348@gmail.com>
2022-09-11 18:25:01 +0100

    Modify countB's to count b's also

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function isEven(n){
 function countBs(str){
   let count = 0;
   for(let i=0; i<str.length; i++){
-    if(str[i] === 'B') count++;
+    if(str[i] === 'B' || str[i] === 'b') count++;
   }
   return count;
 };
